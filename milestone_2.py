631a4887fc9e6145d48a9c0d3a2c787626bbea0a
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-01-31 20:31:27 +0100

    Choosing random word from list

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,12 @@
+import random
+
 ## a list with 5 fruits
 
 word_list = ["apple", "orange", "banana", "pear", "strawberry"]
 
 print(word_list)
 
+## assigning a random word to variable "word"
+word = random.choice(word_list)
+
+print(word)

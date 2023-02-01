f32ccb96e20b880fc4b76fce89bdd366a040e1d7
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-02-01 16:59:41 +0100

    trying to set up constructor

--- milestone_4.py
+++ milestone_4.py
@@ -8,7 +8,7 @@ class Hangman():
 
         #attributes
         self.word_list = word_list
-        self.list_of_guesses = [None]
+        self.list_of_guesses = []
         self.word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))

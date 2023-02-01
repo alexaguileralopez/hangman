82c9f98d816572c5da36014392b107e3ef486011
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-02-01 16:26:55 +0100

    creating hangman class attributes

---
+++ milestone_4.py
@@ -0,0 +1,19 @@
+import random
+
+class Hangman():
+    # class constructor
+    def __init__(self, word_list, num_lives=5): #the number of lives is set to a default value of 5
+
+        #attributes
+        self.word = random.choice(word_list)
+        self.word_guessed = [len(self.word)]
+
+        self.num_letters = len(self.word)
+
+        self.num_lives = num_lives
+        self.word_list = ["apple", "orange", "banana", "pear", "strawberry"]
+        self.list_of_guesses = []
+
+
+
+

49bf62ca728db66e037da3ba53ea2bd1e8c83e0c
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-02-01 16:33:20 +0100

    trying to set up the constructor

--- milestone_4.py
+++ milestone_4.py
@@ -5,14 +5,15 @@ class Hangman():
     def __init__(self, word_list, num_lives=5): #the number of lives is set to a default value of 5
 
         #attributes
+        self.word_list = ["apple", "orange", "banana", "pear", "strawberry"]
+        self.list_of_guesses = []
         self.word = random.choice(word_list)
-        self.word_guessed = [len(self.word)]
+        self.word_guessed = [None] * len(self.word)
 
         self.num_letters = len(self.word)
 
         self.num_lives = num_lives
-        self.word_list = ["apple", "orange", "banana", "pear", "strawberry"]
-        self.list_of_guesses = []
+
 
 
 

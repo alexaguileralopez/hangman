a641c1a61ba19a7ffaf481395409ba13fda4f67a
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-02-01 10:24:13 +0100

    adding if statement of guess in word

--- milestone_3.py
+++ milestone_3.py
@@ -1,9 +1,22 @@
 
+import random
+
+## a list with 5 fruits
+
+word_list = ["apple", "orange", "banana", "pear", "strawberry"]
+
+## assigning a random word to variable "word"
+word = random.choice(word_list)
+
 while True:
     guess = input('Please guess a letter: ')
     if len(guess) == 1 and guess.isalpha() == True:
-        print("Valid")
+        
         break
     else: 
         print("Invalid letter. Please enter a single alphabetical character")
 
+if guess in word:
+    print("Good guess!", guess, "is in the word.")
+else: 
+    print("Sorry,", guess, "is not in the word. Try again")

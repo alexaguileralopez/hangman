b817edc3a5146712008ed5719441e9a0da6914b2
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-02-01 10:40:37 +0100

    milestone 3 but into functions ask input and check guess

--- milestone_3.py
+++ milestone_3.py
@@ -8,15 +8,23 @@ word_list = ["apple", "orange", "banana", "pear", "strawberry"]
 ## assigning a random word to variable "word"
 word = random.choice(word_list)
 
-while True:
-    guess = input('Please guess a letter: ')
-    if len(guess) == 1 and guess.isalpha() == True:
-        
-        break
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print("Good guess!", guess, "is in the word.")
     else: 
-        print("Invalid letter. Please enter a single alphabetical character")
+        print("Sorry,", guess, "is not in the word. Try again")
+    return
 
-if guess in word:
-    print("Good guess!", guess, "is in the word.")
-else: 
-    print("Sorry,", guess, "is not in the word. Try again")
+def ask_for_input():
+    while True:
+        guess = input('Please guess a letter: ')
+        if len(guess) == 1 and guess.isalpha() == True:
+        
+            break
+        else: 
+            print("Invalid letter. Please enter a single alphabetical character")
+    check_guess(guess)
+    return
+
+

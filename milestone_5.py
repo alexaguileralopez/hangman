9f2d4cad68e14564328be604e05c884ede24cc25
alexaguileralopez <alex.aguilera.lopez@gmail.com>
2023-02-05 18:39:41 +0100

    changing num lives for game.num_lives in the play_game function

--- milestone_5.py
+++ milestone_5.py
@@ -63,12 +63,14 @@ def play_game(word_list):
     #game(word_list, num_lives)
 
     while True:
-        if num_lives == 0:
+        if game.num_lives == 0:
             print("You lost!")
             break
+
         elif game.num_letters > 0:
             game.ask_for_input()
-        elif num_lives != 0 and game.num_letters < 0:
+
+        elif game.num_lives != 0 and game.num_letters  <= 0:
             print("Congratulations! You won the game!")
             break
     return
